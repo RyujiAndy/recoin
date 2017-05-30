38d8e07b387af93f72ea8b8028268cc2e562a78f
RyujiAndy <ryujiandy@i2cttl.com>
2017-05-30 21:24:40 +0200

    Init

--- build/release/external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stomeo-gmbh/Programme/recoin-qt/cryptonote/external
-# Build directory: /home/stomeo-gmbh/Programme/recoin-qt/cryptonote/build/release/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(miniupnpc)
-subdirs(gtest)
--- build/release/external/miniupnpc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stomeo-gmbh/Programme/recoin-qt/cryptonote/external/miniupnpc
-# Build directory: /home/stomeo-gmbh/Programme/recoin-qt/cryptonote/build/release/external/miniupnpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
